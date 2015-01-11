361cbe0790ed0ff943b0be3a755cba418c461268
thiagopnts <thiagopnts@gmail.com>
2015-01-11 17:34:58 -0200

    implement some ops

---
+++ src/brainfuck.rs
@@ -0,0 +1,135 @@
+use std::io::{stdin, Reader};
+use std::ascii::AsciiExt;
+
+#[deriving(PartialEq)]
+enum Op {
+    Left      = 60,
+    Right     = 62,
+    Increment = 43,
+    Decrement = 45,
+    Output    = 46,
+    Input     = 44,
+    Jump      = 91,
+    JumpBack  = 93,
+    Unknown,
+}
+
+trait ToOp {
+    fn to_op(&self) -> Op;
+}
+
+impl ToOp for u8 {
+    fn to_op(&self) -> Op {
+        match *self {
+            60 => Op::Left,
+            62 => Op::Right,
+            43 => Op::Increment,
+            45 => Op::Decrement,
+            46 => Op::Output,
+            44 => Op::Input,
+            91 => Op::Jump,
+            93 => Op::JumpBack,
+            _  => Op::Unknown,
+        }
+    }
+}
+
+pub struct Brainfuck {
+    memory: Vec<u8>,
+    content: Box<Reader + 'static>,
+    pointer: i64,
+}
+
+impl Brainfuck {
+    pub fn new(content: Box<Reader + 'static>) -> Brainfuck {
+        let mut v = vec!();
+        for i in range(0, 10000) {
+            v.push(64);
+        }
+        Brainfuck { memory: v, content: content, pointer: 0 }
+    }
+    pub fn evaluate(&mut self) {
+        'eval: loop {
+            match self.content.read_byte() {
+                Ok(byte) => match byte.to_op() {
+                    Op::Left      => self.left(),
+                    Op::Right     => self.right(),
+                    Op::Increment => self.increment(),
+                    Op::Decrement => self.decrement(),
+                    Op::Output    => self.output(),
+                    Op::Input     => self.input(),
+                    Op::Jump      => self.jump(),
+                    Op::JumpBack  => self.jump_back(),
+                    Op::Unknown => self.unknown(),
+                },
+                Err(err) => break 'eval
+            }
+        }
+        println!("");
+        println!("done evaluating");
+    }
+
+    fn left(&mut self) {
+        self.pointer -= 1;
+    }
+
+    fn right(&mut self) {
+        self.pointer += 1;
+    }
+
+    fn increment(&mut self) {
+        self.memory[self.pointer as usize] += 1;
+    }
+
+    fn decrement(&mut self) {
+        self.memory[self.pointer as usize] -= 1;
+    }
+
+    fn output(&self) {
+        print!("{}", (self.memory[self.pointer as usize].to_ascii_lowercase()) as char);
+    }
+
+    fn input(&self) {
+        let c = stdin().read_char().ok().expect("Error reading user input");
+    }
+
+    fn jump(&mut self) {
+        let mut bal = 1;
+        if self.memory[self.pointer as usize] as char == '\0' {
+            'foo: loop {
+                self.pointer += 1;
+                let c = self.memory[self.pointer as usize] as char;
+                if c == '[' {
+                    bal += 1;
+                } else if c == ']' {
+                    bal -= 1;
+                }
+                if bal == 0 {
+                    break 'foo
+                }
+            }
+        }
+    }
+
+    fn jump_back(&mut self) {
+        let mut bal = 0;
+        if self.memory[self.pointer as usize] as char == '\0' {
+            'foo: loop {
+                let c = self.memory[self.pointer as usize] as char;
+                if c == '[' {
+                    bal += 1;
+                } else if c == ']' {
+                    bal -= 1;
+                }
+                self.pointer -= 1;
+                if bal == 0 {
+                    break 'foo
+                }
+            }
+        }
+    }
+
+    fn unknown(&self) {
+        println!("\nunknown op");
+    }
+}
--- src/repl.rs
+++ src/repl.rs
@@ -1,15 +1,18 @@
 use std::io;
 
+#[path = "brainfuck.rs"]
+mod interpreter;
+
 pub struct Repl;
 
 impl Repl {
     pub fn run() {
-        let mut reader = io::stdin();
         println!("\n\n\nBrainfuck REPL version 0.0.1");
         loop {
             print!("> ");
-            let code = reader.read_line().ok().expect("Failed to read line");
-            println!("{}", code);
+            let mut reader = io::stdin();
+            let mut brainfuck = interpreter::Brainfuck::new(Box::new(reader));
+            brainfuck.evaluate();
         }
     }
 }
