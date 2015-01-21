7f6ec61369c8e2ace9eec33c6940158b45d4ad98
thiagopnts <thiagopnts@gmail.com>
2015-01-21 21:24:59 -0200

    Revert "evalutate file passed as argument"
    This reverts commit 75c6d11468b21449b2e9c566c8ec9c43173d043a.

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ mod repl;
 fn main() {
     let arguments = os::args();
     match arguments.get(1) {
-        Some(ref name) => repl::Repl::evaluate(File::new(&Path::new(name))),
+        Some(ref name) => println!("file: {}", name),
         None => repl::Repl::run(),
     }
 }
--- src/repl.rs
+++ src/repl.rs
@@ -8,11 +8,6 @@ mod interpreter;
 pub struct Repl;
 
 impl Repl {
-    pub fn evaluate(reader: Reader) {
-        let mut brainfuck = interpreter::Brainfuck::new(Box::new(reader));
-        brainfuck.evaluate();
-    }
-
     pub fn run() {
         println!("\n\n\nBrainfuck REPL version 0.0.1");
         loop {
